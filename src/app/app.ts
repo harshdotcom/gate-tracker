dee06c9ebe2c314ec9280ef0a8f1d27333edd037
Harsh Jha <harshj92002@gmail.com>
2026-06-08 11:22:08 +0530

    updated route

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('gate-civil-tracker');
+  protected readonly title = signal('gate-tracker');
 }
